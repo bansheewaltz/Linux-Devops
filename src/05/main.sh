c20c7c082d26615d53e8115c8046bf9397560832
Anton <grevtsov.apn@gmail.com>
2023-07-18 13:54:25 +0700

    part5 ready

--- src/05/main.sh
+++ src/05/main.sh
@@ -30,16 +30,16 @@ echo "TOP 5 folders of maximum size arranged in descending order (path and size)
       sudo du -hS "$path" | sort -hr | nl | awk '{printf "%d - %s, %sB\n", $1, $3, $2}' | head -5
 echo "Total number of files = $(sudo find "$path" -mindepth 1 -type f | wc -l)"
 echo "Number of:"
-echo "Configuration files (with the .conf extension) = $(sudo find "$path" -type f -name '*.conf' | wc -l)"
-echo "Text files = $(sudo find "$path" -type f -exec file {} + | grep text | wc -l)"
-echo "Executable files = $(sudo find "$path" -type f -executable | wc -l)"
-echo "Log files (with the extension .log) = $(sudo find "$path" -type f -name '*.log' | wc -l)"
-echo "Archive files = $(sudo find "$path" -type f -exec file {} + | grep -E "compressed|archive"| wc -l)"
-echo "Symbolic links = $(sudo find -L "$path" -type f -exec file {} + | grep symbolic | wc -l)"
+      echo "Configuration files (with the .conf extension) = $(sudo find "$path" -type f -name '*.conf' | wc -l)"
+      echo "Text files = $(sudo find "$path" -type f -exec file {} + | grep text | wc -l)"
+      echo "Executable files = $(sudo find "$path" -type f -executable | wc -l)"
+      echo "Log files (with the extension .log) = $(sudo find "$path" -type f -name '*.log' | wc -l)"
+      echo "Archive files = $(sudo find "$path" -type f -exec file {} + | grep -E "compressed|archive"| wc -l)"
+      echo "Symbolic links = $(sudo find "$path" -type l -executable | wc -l)"
 echo "TOP 10 files of maximum size arranged in descending order (path, size and type):"
       sudo find "$path" -type f -exec du -hS {} + | sort -hr | nl | head | awk '{printf "%d - %s, %sB\n", $1, $3, $2}'
 echo "TOP 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file) "
       sudo find "$path" -type f -executable -exec du -sh {} + | sort -hr | nl | head | awk '{cmd = "md5sum " $3; cmd|getline result; printf "%d - %s, %sB, %s\n", $1, $3, $2, result}' | awk '{print $1,$2,$3,$4,$5}'
 time_end=$(date +%s.%N)
 
-echo "Script execution time (in seconds) =" $(time_elapsed $time_start $time_end)
+echo "Script execution time (in seconds) = $(time_elapsed $time_start $time_end)"
