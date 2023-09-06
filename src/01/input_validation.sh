724d8bcd394eeed224802d5b511a92a22b4bcb14
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-09-06 20:17:02 +0700

    src:01:input: add character uniqueness check and reduce regex prefix

--- src/01/input_validation.sh
+++ src/01/input_validation.sh
@@ -33,9 +33,9 @@ function validate_input {
     terminate "The script takes exactly 6 arguments as input"; fi
   
   # first parameter
-  regex_abs_path='^/[a-zA-Z0-9/_-]*[^/]$'
-  regex_root='^/$'
-  if [[ ! $path =~ $regex_abs_path && ! $path =~ $regex_root ]]; then
+  re_abs_path='^/[a-zA-Z0-9/_-]*[^/]$'
+  re_root='^/$'
+  if [[ ! $path =~ $re_abs_path && ! $path =~ $re_root ]]; then
     terminate "The first parameter, the path, must be an absolute path to a \
                directory, without \"/\" in the end"; fi
   if [[ ! -d $path ]]; then
@@ -44,35 +44,45 @@ function validate_input {
     terminate "Permission denied for '$path'"; fi
   
   # second parameter
-  regex_subdir_count='^0*[1-9][0-9]*$'
-  if [[ ! $dir_count =~ $regex_subdir_count ]]; then
+  re_subdir_count='^0*[1-9][0-9]*$'
+  if [[ ! $dir_count =~ $re_subdir_count ]]; then
     terminate "The second parameter, the folder count, accepts only numbers \
                and the value should be at least 1"; fi  
 
   # third parameter
-  regex_dir_letters='^[A-Za-z]{1,7}$'
-  if [[ ! $dir_letters =~ $regex_dir_letters ]]; then
+  re_repeated_characters='\(.\).*\1'
+  re_dir_letters='^[A-Za-z]{1,7}$'
+  if [[ ! $dir_letters =~ $re_dir_letters ]]; then
     terminate "The third parameter, the folder's name, must contain no more \
-               than 7 characters of [A-Z], [a-z] without repetition"; fi
+               than 7 characters of English alphabet"; fi
+  if echo "$dir_letters" | grep -q $re_repeated_characters ; then  
+    terminate "The third parameter, the folder's name, should not contain \
+               repetition of characters"; fi
 
   # fourth parameter
-  regex_file_count='^0*[1-9][0-9]*$'
-  if [[ ! $file_count =~ $regex_file_count ]]; then
+  re_file_count='^0*[1-9][0-9]*$'
+  if [[ ! $file_count =~ $re_file_count ]]; then
     terminate "The fourth parameter, the file count, accepts only numbers \
                and the value should be at least 1"; fi 
 
   # fifth parameter
-  regex_file_name='^[A-Za-z]{1,7}\.[A-Za-z]{1,3}$'
-  if [[ ! $file_letters =~ $regex_file_name ]]; then
+  re_file_name='^[A-Za-z]{1,7}\.[A-Za-z]{1,3}$'
+  if [[ ! $file_letters =~ $re_file_name ]]; then
     terminate "The fifth parameter, a file\`s name, must contain only A-z, \
                a-z letters and you can use not more 7 letters for a name and \
                not more 3 letters for \
                an extension. Letters in a name and in an extension can\`t \
                repeat. Example: sdf.ex"; fi
+  if echo "$bn_letters" | grep -q $re_repeated_characters ; then  
+    terminate "In the fifth parameter, a file\`s basename, should not contain \
+               repetition of characters"; fi
+  if echo "$ext_letters" | grep -q $re_repeated_characters ; then  
+    terminate "In the fifth parameter, a file\`s extension, should not contain \
+               repetition of characters"; fi
 
   # sixth parameter
-  regex_file_size_kib='^0*[1-9][0-9]*[kK][bB]$'
-  if [[ ! $file_size_kib =~ $regex_file_size_kib ]]; then
+  re_file_size_kib='^0*[1-9][0-9]*[kK][bB]$'
+  if [[ ! $file_size_kib =~ $re_file_size_kib ]]; then
     terminate "The sixth parameter, the file size, must be in kb and has \
                at least 1 as a value. Example: 23kb."; fi
   file_size_kib=$(echo $file_size_kib | sed 's/'[kK][bB]'//')
