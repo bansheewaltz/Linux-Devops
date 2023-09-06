5feee7f86deed1d3e9c6dc47e7b24bb2ca56985a
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-09-06 16:16:05 +0700

    instrumenting: update docker run script

--- src/run_container.sh
+++ src/run_container.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-image=monitoring2:dondarri
-
-if [ ! "$(docker images -q $image)" ]; then
-  docker build -t $image ubuntu .
-fi
-
 docker run -it --rm \
            -v "$PWD"/:/home/ \
            -w /home \
-           -m 1712m \
-           --name monitoring2_test \
-           $image
+           -m 1112m \
+           --name monitoring2_dondarri \
+           ubuntu
