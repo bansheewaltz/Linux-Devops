63f63a599cb3ab1bdbae71d1283c35c147737c70
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-09-09 02:11:05 +0700

    src:03:main: decompose + add verbose version of method 3

--- src/03/main.sh
+++ src/03/main.sh
@@ -62,11 +62,7 @@ if [ "$method" -eq 2 ]; then
 fi
 
 # Cleanup by timestamp
-if [ "$method" -eq 3 ]; then
-  echo "Input the filefolder name mask (i.e. characters, underlining and date)"
-  printf -v date_suffix '%(%d%m%y)T'
-  echo "Example: ab_${date_suffix}"
-  echo
+function read_mask {
   read -r name_mask
   re_name_general='^[a-zA-Z]{1,7}_[0-9]{6}$'
   re_repeated_chars='([a-zA-Z]).*?\1'
@@ -76,15 +72,50 @@ if [ "$method" -eq 3 ]; then
     if [ "$name_mask" = exit ]; then
       exit 0; fi
   done
+}
 
+function find_files_by_mask() {
+  name_mask=$1
   letters=${name_mask%%_*}
   date=${name_mask##*_}
-  declare re_letters
+  re_letters=
   for (( i=0; i<${#letters}; i++ )); do
     re_letters+=${letters:$i:1}+; done
   mask_re=".*/${re_letters}_${date}"
   filefolders=$(find / -type d -regex "$mask_re")
   if [ "$filefolders" ]; then
-    files=$(find $filefolders -type f); fi
+    files=$(find $filefolders -type f);
+  else
+    files=
+  fi
   echo "$files"
+}
+
+if [ "$method" -eq 3 ]; then
+  echo "Input the filefolder name mask (i.e. characters, underlining and date)"
+  printf -v date_suffix '%(%d%m%y)T'
+  echo "Example: ab_${date_suffix}"
+  echo
+  
+  lines_to_remove=0
+  while true; do
+    read_mask
+    files=$(find_files_by_mask "$name_mask")
+    files_n=$(echo "$files" | wc -l)
+    echo
+    echo "files found:"
+    echo "$files"
+    echo
+    lines_to_remove=5
+    while true; do
+      read -p "Do you wish to remove the above files?[Y/n] " yn
+      case $yn in
+          [Yy]* ) cleanup "$files" ; exit 0;;
+          [Nn]* ) break;;
+          * ) echo "Please answer yes or no."; ((lines_to_remove+=2));;
+      esac
+    done
+    printf "\033[%sA" $(($lines_to_remove + $files_n))
+    printf "\033[J"
+  done
 fi
