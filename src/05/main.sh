4a919db54cd1879a0560ce45549d9dcaff1c91d5
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-09-09 19:29:28 +0700

    src:05: use long version of commands' options for the sake of clarity

--- src/05/main.sh
+++ src/05/main.sh
@@ -3,32 +3,23 @@ source parameter_validation.sh
 
 arg_count=$#
 parameter=$1
+logs=$(cat ../04/logs/*.log 2>/dev/null)
 
 validate_parameter
 
 
-logs=$(cat ../04/logs/*.log)
-
 if [ "$parameter" -eq 1 ]; then
-  echo "$logs" | sort -nk 9
+  echo "$logs" | sort --numeric-sort --key=9
 fi
 
 if [ "$parameter" -eq 2 ]; then
-  echo "$logs" | awk '{print $1}' | sort -u
+  echo "$logs" | awk '{ print $1 }' | sort --unique
 fi
 
 if [ "$parameter" -eq 3 ]; then
-  echo "$logs" | awk '{
-    if (match($9, /^[4,5]/) != 0) {
-      print $0
-    }
-  }'
+  echo "$logs" | awk '{ if (match($9, /^[4,5]/)) print $0 }'
 fi
 
 if [ "$parameter" -eq 4 ]; then
-  echo "$logs" | awk '{
-    if (match($9, /^[4,5]/) != 0) {
-      print $1
-    }
-  }' | sort -u
+  echo "$logs" | awk '{ if (match($9, /^[4,5]/)) print $0 }' | sort --unique
 fi
