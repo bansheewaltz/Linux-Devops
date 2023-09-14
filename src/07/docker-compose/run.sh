cb0e37a021608cb520f0879840d80478f15cc9c3
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-09-14 23:49:42 +0700

    src:07:compose: add runner script

--- src/07/docker-compose/run.sh
+++ src/07/docker-compose/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 if [ "$1" = clean ]; then
-  rm -rf grafana prometheus;
+  rm -rf grafana prometheus
+  docker-compose down
 else
-  mkdir -p prometheus grafana/provisioning
+  mkdir -p prometheus grafana/provisioning/{dashboards,datasources}
+  cp dashboard* grafana/provisioning/dashboards
+  cp datasources.* grafana/provisioning/datasources
+  docker-compose up -d
+  docker exec -it test-env bash
 fi
