16c47cddb5ead3db8f3137b4163eba848554c41f
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-09-16 15:16:38 +0700

    ci:notify: set bold style for job status

--- notify.sh
+++ notify.sh
@@ -9,9 +9,12 @@ if [ "$CI_JOB_STATUS" = success ] && ! [ "$CI_JOB_STAGE" = deploy ]; then
   exit 0;
 fi
 
+
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+CI_COMMIT_AUTHOR=${CI_COMMIT_AUTHOR//[<>]/}
+CI_JOB_STATUS=${CI_JOB_STATUS^^}
 TEXT="
-$CI_JOB_STAGE:$CI_JOB_NAME status: $CI_JOB_STATUS%0A%0A\
+$CI_JOB_STAGE:$CI_JOB_NAME status: <b>$CI_JOB_STATUS</b>%0A\
 Project:+$CI_PROJECT_NAME%0A\
 URL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0A\
 Branch:+$CI_COMMIT_REF_SLUG%0A\
@@ -20,6 +23,17 @@ Commit message:%0A\
 $CI_COMMIT_MESSAGE"
 
 TIME=10
-curl -s --max-time $TIME \
-     -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" \
-     $URL > /dev/null
+#  -s \
+#  > /dev/null
+curl \
+ --max-time $TIME \
+ --data parse_mode=HTML \
+ --data chat_id=$TELEGRAM_USER_ID \
+ --data text="$TEXT" \
+ --data disable_web_page_preview=1 \
+ --request POST $URL \
+
+echo $TEXT
+# curl -s --max-time $TIME \
+#      -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" \
+#      $URL > /dev/null
