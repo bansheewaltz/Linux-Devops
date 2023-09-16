23ed20d8626a5af433547e476d938662d58dfa04
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-09-16 21:39:05 +0700

    ci:deploy: add ssh commands

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+ssh-keygen -t rsa -N "" -f /root/.ssh/vm_key
+sshpass -p1 ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/vm_key.pub root@vm
+
 BIN_DIR=build/bin
 scp $BIN_DIR/* root@vm:/usr/local/bin
