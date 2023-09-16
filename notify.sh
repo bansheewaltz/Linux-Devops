8b23b9f26f67eeddde1a99008475bd5d92fee571
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-09-16 13:32:38 +0700

    ci:notify: break the text to make it more readable

--- notify.sh
+++ notify.sh
@@ -6,7 +6,13 @@ TELEGRAM_USER_ID=71115111
 TELEGRAM_BOT_TOKEN=6627077999:AAEaRjYRMsF27MhpIv8Jdwb9b8hXDd3vVKc
 
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="
+Deploy status: $1%0A%0A\
+Project:+$CI_PROJECT_NAME%0A\
+URL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0A\
+Branch:+$CI_COMMIT_REF_SLUG"
 
 TIME=10
-curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
+curl -s --max-time $TIME \
+     -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" \
+     $URL > /dev/null
